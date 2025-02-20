58666966f2dc5987b76ee531c7c4800592813c4c
rebelshsjjs <yuhgfgjjjggjjj@gmail.com>
2025-02-20 18:27:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,12 +28,12 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7770278507:AAHI9T-sdDEbvtAxFMNSrTiA1PImNXi
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/56b5deb73f3b132e2bb73.jpg https://graph.org/file/5303692652d91d52180c2.jpg https://graph.org/file/425b6f46efc7c6d64105f.jpg https://graph.org/file/876867e761c6c7a29855b.jpg')).split()  # Sample pic
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/60e8a622b14796e4448ce.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/242b7f1b52743938d81f1.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
+PICS = (environ.get('PICS', 'https://envs.sh/dcn.jpg')).split()  # Sample pic
+NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/dcn.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/dcn.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/dcn.jpg")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/dcn.jpg'))
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://envs.sh/dcn.jpg')).split()  # Fsub pic
 
 # ============================
 # Admin, Channels & Users Configuration
